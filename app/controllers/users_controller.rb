e8c3e3fc90e20ce6ac3c898dc2dae71f87d43a29
Ryo-cool <coolboxing576@gmail.com>
2020-07-17 19:31:35 +0900

    ユーザー情報編集
    gem 'carrierwave'
    gem 'mini_magick'
    を導入し、ユーザー情報に画像を追加できるように実装した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :introduction, :image)
   end
 
   def set_user
