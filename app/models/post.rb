7e646efd62c9713e3a6208688ac70be1ec4b115f
Ryo-cool <coolboxing576@gmail.com>
2020-07-28 13:12:33 +0900

    postモデルバリデーション
    名前、本文、画像の制約を付与

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
+  # バリデーション
+  validates :name, presence: true, length: { maximum: 40}
+  validates :images,presence: true
+  validates :text, presence: true, length: { maximum: 1000 }
 end
