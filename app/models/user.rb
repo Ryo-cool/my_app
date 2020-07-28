1d7b1c61b3aff683fe0c4a864a2a381172df7443
Ryo-cool <coolboxing576@gmail.com>
2020-07-28 14:43:22 +0900

    コメントモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: :user_id, dependent: :destroy
+  has_many :comments
   validates :name,        uniqueness: true
   validates :email,            uniqueness: true
 end
