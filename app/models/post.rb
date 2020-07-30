c087afd63471672b926f5dacb8b1b29c8f5daf3e
Ryo-cool <coolboxing576@gmail.com>
2020-07-30 15:24:51 +0900

    いいねボタン表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :images, dependent: :destroy
   has_many :comments
+  has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   # バリデーション
   validates :name, presence: true, length: { maximum: 40}
   validates :images,presence: true
   validates :text, presence: true, length: { maximum: 1000 }
+
+  def like_user(id)
+    likes.find_by(user_id: id)
+  end
 end
