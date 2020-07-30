35a7ffcada791b5eadfb30eb7a7ea5120f5ca45f
Ryo-cool <coolboxing576@gmail.com>
2020-07-30 13:07:16 +0900

    Revert "ページネーション機能の実装"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :show, :destroy]
   def index
     @posts = Post.all
-    @posts = Post.includes(:images).order('created_at DESC').page(params[:page]).per(5)
+    @posts = Post.includes(:images).order('created_at DESC').limit(9)
     @images = Image.all
   end
 
