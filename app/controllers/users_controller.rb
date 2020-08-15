9bca0fb7c1e2db3f8b37b45f05a728409b95f9eb
Ryo-cool <coolboxing576@gmail.com>
2020-08-16 01:50:42 +0900

    コントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ class UsersController < ApplicationController
   def index
     @users=User.all
   end
+  
   def show
-    #user = User.find(params[:id])
+    user = User.find(params[:id])
     @nickname = current_user.name
+    # @user = @post.user
+    @posts = @user.posts
   end
   
   
