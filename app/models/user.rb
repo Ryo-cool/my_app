640924a9334d257b0f9ba365b24d694d5f8e9f97
Ryo-cool <coolboxing576@gmail.com>
2020-08-17 18:47:39 +0900

    userモデル編集
    ログイン・ログアウトを繰り返し行えるよう追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.confirmed_at = Time.now
     end
   end
-  
+
 end
