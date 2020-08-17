0d3402849c28912772ddfc0c6f45f56a842a8dd8
Ryo-cool <coolboxing576@gmail.com>
2020-08-17 18:22:41 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :posts do
     resources :comments,only: [:create,:destroy]
     resources :likes, only: [:create, :destroy]
